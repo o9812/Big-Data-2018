044901442dd79e8e33c44382b1f4b3d9982c2dd8
goandseeleah <31750425+goandseeleah@users.noreply.github.com>
2018-05-14 01:18:31 -0400

    update

--- greenday/datetimetransformer.py
+++ greenday/datetimetransformer.py
@@ -4,9 +4,7 @@ from pyspark.sql.types import FloatType, DateType, IntegerType
 from pyspark.sql.functions import unix_timestamp, from_unixtime, current_date, datediff, col, dayofmonth, year, month
 import pandas as pd
 
-
-
-class datetimetransformer:
+class DateTimeTransformer:
 
     #constructor
     def __init__(self, df):
@@ -50,21 +48,24 @@ class datetimetransformer:
         assert len(columns) == len (new_col), "Error: inconsistent lengths for argument of columns list and new columns list"
 
         for i in range(len(columns)):
-            self._df = self._df.select(columns[i], from_unixtime(unix_timestamp(columns[i], 'MM/dd/yyy')).alias('with time'))
+
+            df = self._df.select(columns[i], from_unixtime(unix_timestamp(columns[i], 'MM/dd/yyy')).alias('with time'))
+            df=df.withColumn('without time', df['with time'].cast(DateType()))
+            df=df.withColumnRenamed(columns[i], 'tmp')
+            self._df = self._df.join(df, col(columns[i])==col('tmp'), 'leftouter').drop('tmp')    
+
             if stripTime:
-                if Inplace: #strip time and in place change
-                    self._df = self._df.withColumn('without time', self._df['with time'].cast(DateType()))
-                    self._df = self._df.drop(columns[i]).drop('with time').withColumnRenamed('without time', new_col[i])
+                if Inplace: #strip time and convert in place    
+                    self._df = self._df.withColumnRenamed('without time', new_col[i]).drop('with time').drop(columns[i])
                 else: #strip time and keep the old column
-                    self._df = self._df.withColumn('without time', self._df['with time'].cast(DateType()))
-                    self._df = self._df.drop('with time').withColumnRenamed('without time', new_col[i])
+                    self._df = self._df.withColumnRenamed('without time', new_col[i]).drop('with time')
             else:
                 if Inplace:
-                    self._df = self._df.drop(columns[i]).withColumnRenamed("with time", new_col[i])
+                    self._df = self._df.withColumnRenamed('with time', new_col[i]).drop('without time').drop(columns[i])
                 else:
-                    self._df = self._df.withColumnRenamed("with time", new_col[i])
+                    self._df = self._df.withColumnRenamed('with time', new_col[i]).drop('without time')
 
-        return self._df
+        return self
 
 
     def age_calculator(self, columns, age_colname):
@@ -75,10 +76,13 @@ class datetimetransformer:
         assert isinstance(columns, str), "Error: columns argument must be a string!"
         assert isinstance(age_colname, str), "Error: the name for age column argument must be a string!"
 
-        self._df = (self.str_to_Date(columns, columns, stripTime=True, Inplace=True))._df
-        self._df = self._df.withColumn(age_colname, ((datediff(current_date(), col(columns)).cast(FloatType()))/365).cast(IntegerType()))
-        
-        return self._df
+        df = self._df.select(columns, from_unixtime(unix_timestamp(columns, 'MM/dd/yyy')).alias('with time'))
+        df=df.withColumn('without time', df['with time'].cast(DateType()))
+        df=df.withColumnRenamed(columns, 'tmp')
+        df=df.withColumn(age_colname, ((datediff(current_date(), col('without time')).cast(FloatType()))/365).cast(IntegerType()))
+        self._df = self._df.join(df, col(columns)==col('tmp'), 'leftouter').drop('tmp').drop('with time').drop('without time')
+
+        return self
 
 
     def Date_spliter(self, columns, y_col, m_col, d_col):
@@ -98,9 +102,11 @@ class datetimetransformer:
         self._df = (self.str_to_Date(columns, columns, stripTime=True, Inplace=True))._df
         self._df = self._df.withColumn(y_col, year(self._df[columns])).withColumn(m_col,month(self._df[columns])).withColumn(d_col,dayofmonth(self._df[columns]))
 
-        return self._df
 
+        df = self._df.select(columns, from_unixtime(unix_timestamp(columns, 'MM/dd/yyy')).alias('with time'))
+        df=df.withColumn('without time', df['with time'].cast(DateType()))
+        df=df.withColumnRenamed(columns, 'tmp')
+        self._df = self._df.join(df, col(columns)==col('tmp'), 'leftouter').drop('tmp')   
+         
 
-
-
-
+        return self
